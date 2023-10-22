66fa8da526382c9c06c0951167ded128cc68a472
EdiedRamos <edieisa22@gmail.com>
2023-10-22 08:50:38 -0500

    remove bad direction in atoms index.js

--- src/Components/Atoms/index.ts
+++ src/Components/Atoms/index.ts
@@ -1,5 +1,4 @@
 export { MyFace } from "./MyFace/MyFace";
-export { MyName } from "./MyName/MyName";
 export { Card } from "./Card/Card";
 export { ContactText } from "./ContactText/ContactText";
 export { BorderContainer } from "./BorderContainer/BorderContainer";
