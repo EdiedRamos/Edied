5c50fbf92d9c3e6cc97bd387c00756280d4970f5
EdiedRamos <edieisa22@gmail.com>
2023-11-25 11:34:53 -0500

    update years of experience

--- src/Components/Molecules/IntroduceMyself/IntroduceMyself.tsx
+++ src/Components/Molecules/IntroduceMyself/IntroduceMyself.tsx
@@ -11,7 +11,7 @@ export const IntroduceMyself = () => {
       <Paragraph>
         Hello! I'm <Span color="#FFD494">Edied </Span>{" "}
         <Span color="#FFD494">Isaías </Span> <Span color="#1CAEFD">Ramos </Span>{" "}
-        <Span color="#F9847B">Hernández</Span>, a web developer with two years
+        <Span color="#F9847B">Hernández</Span>, a web developer with three years
         of experience passionate about building high-quality applications and
         efficiently solving problems. My focus is on creating robust and
         optimized web solutions, combining my love for algorithms with a
