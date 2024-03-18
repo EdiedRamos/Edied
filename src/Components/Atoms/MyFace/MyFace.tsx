f2281f25648b98013892602cea93266b8d271652
EdiedRamos <edieisa22@gmail.com>
2024-03-18 01:03:15 -0500

    chore: update face photo dimensions

--- src/Components/Atoms/MyFace/MyFace.tsx
+++ src/Components/Atoms/MyFace/MyFace.tsx
@@ -5,7 +5,7 @@ import "./MyFace.scss";
 export const MyFace = () => {
   return (
     <div className="my-face-container">
-      <img width="250" height="240" src={Profile} alt="Edied's Face" />
+      <img width="250" height="250" src={Profile} alt="Edied's Face" />
     </div>
   );
 };
