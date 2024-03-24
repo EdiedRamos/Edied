ad46ef782acc634577ae7e170006e7d14b403140
EdiedRamos <edieisa22@gmail.com>
2024-03-24 17:20:13 -0500

    chore: add new project

--- src/Resources/Projects/index.ts
+++ src/Resources/Projects/index.ts
@@ -1,9 +1,10 @@
+export { default as AdviceGenerator } from "./advicegenerator.webp";
 export { default as CountryListing } from "./countrylisting.webp";
 export { default as Creditos } from "./creditos.webp";
-export { default as JobListing } from "./joblisting.webp";
-export { default as AdviceGenerator } from "./advicegenerator.webp";
-export { default as NotificationPage } from "./notificationpage.webp";
 export { default as ExpensesChart } from "./expenseschart.webp";
 export { default as FourCardFeatureSection } from "./fourcardfeaturesection.webp";
-export { default as SinglePrice } from "./singleprice.webp";
 export { default as IpTracker } from "./ip-tracker.webp";
+export { default as JobListing } from "./joblisting.webp";
+export { default as NotificationPage } from "./notificationpage.webp";
+export { default as QrGenerator } from "./qr-generator.webp";
+export { default as SinglePrice } from "./singleprice.webp";
