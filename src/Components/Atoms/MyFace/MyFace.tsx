35762aa6f8d4ff0519800b530a7111b35eefae87
EdiedRamos <edieisa22@gmail.com>
2024-04-18 19:14:16 -0500

    style: myface styles updated

--- src/Components/Atoms/MyFace/MyFace.tsx
+++ src/Components/Atoms/MyFace/MyFace.tsx
@@ -4,8 +4,16 @@ import "./MyFace.scss";
 
 export const MyFace = () => {
   return (
-    <div className="my-face-container">
-      <img width="250" height="240" src={Profile} alt="Edied's Face" />
+    <div className="my-face my-face__container">
+      <div className="my-face__box">
+        <img
+          className="my-face__img"
+          width="250"
+          height="240"
+          src={Profile}
+          alt="Edied's Face"
+        />
+      </div>
     </div>
   );
 };
