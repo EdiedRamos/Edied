9f8e27029e29d13a6a241bc4da2b20da75e8e7d1
EdiedRamos <edieisa22@gmail.com>
2024-06-18 01:12:13 -0500

    feat: update cv

--- src/Data/Contact/index.ts
+++ src/Data/Contact/index.ts
@@ -1,6 +1,6 @@
-import type { IconType } from "react-icons";
-
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+import type { IconType } from "react-icons";
 import { SiGmail } from "react-icons/si";
 import { TbFileCv } from "react-icons/tb";
 
@@ -28,7 +28,7 @@ export const contactList: ContactT[] = [
   },
   {
     name: "cv",
-    href: "https://drive.google.com/file/d/1Dd2xbO16LNYmlAmpgcl-P5ICs6eoSTi2/view?usp=sharing",
+    href: "https://drive.google.com/file/d/18Zw_IV6JV3j6t5B0LYGSMP209AQjAohT/view?usp=sharing",
     Icon: TbFileCv,
   },
 ];
