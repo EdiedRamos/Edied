d1b00fcd8d7a98f737c0bff43ce52c00c66927de
EdiedRamos <edieisa22@gmail.com>
2024-07-13 02:39:58 -0500

    feat: configure i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
+import "./General/Styles/main.scss";
+import "./i18n";
+
+import App from "@/App.tsx";
+import { I18nextProvider } from "react-i18next";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "@/App.tsx";
-
-// import "@/General/Styles/main.scss";
-
-import "./General/Styles/main.scss";
+import i18n from "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
