ea9cb8d226d3557b1f96c7257b7c3a9414abd356
EdiedRamos <edieisa22@gmail.com>
2024-07-13 01:43:04 -0500

    feat: update projects and set the top 6

--- src/Data/Projects/index.ts
+++ src/Data/Projects/index.ts
@@ -1,15 +1,8 @@
 import {
-  Boardify,
   CountryQuiz,
-  GithubProfile,
-  GuessTheWord,
   ImageUpload,
-  IpTracker,
-  MusicPlayer,
   MyTaskBoard,
   NoteCode,
-  QrGenerator,
-  TicTacToe,
   UrlShortener,
   WorldRanks,
 } from "@/Resources/Projects";
@@ -99,6 +92,26 @@ export const projectList: ProjectCardType[] = [
     ],
   },
   {
+    title: "Url Shortener",
+    preview: UrlShortener,
+    repository: "https://github.com/EdiedRamos/url-shortening-api-landing-page",
+    deployed: "https://url-shortening-api-landing-page-tawny.vercel.app/",
+    techList: [
+      {
+        name: "react",
+        Icon: Icons.react,
+      },
+      {
+        name: "typescript",
+        Icon: Icons.typescript,
+      },
+      {
+        name: "sass",
+        Icon: Icons.sass,
+      },
+    ],
+  },
+  {
     title: "World Ranks",
     preview: WorldRanks,
     repository: "https://github.com/EdiedRamos/country-page",
@@ -127,66 +140,6 @@ export const projectList: ProjectCardType[] = [
     ],
   },
   {
-    title: "Github Profile",
-    preview: GithubProfile,
-    repository: "https://github.com/EdiedRamos/github-profile",
-    deployed: "https://github-profile-five-tau.vercel.app/",
-    techList: [
-      {
-        name: "react",
-        Icon: Icons.react,
-      },
-      {
-        name: "typescript",
-        Icon: Icons.typescript,
-      },
-      {
-        name: "tailwind",
-        Icon: Icons.tailwind,
-      },
-    ],
-  },
-  {
-    title: "Boardify",
-    preview: Boardify,
-    repository: "https://github.com/EdiedRamos/boardify",
-    deployed: "https://boardify-psi.vercel.app/",
-    techList: [
-      {
-        name: "react",
-        Icon: Icons.react,
-      },
-      {
-        name: "typescript",
-        Icon: Icons.typescript,
-      },
-      {
-        name: "chakraui",
-        Icon: Icons.chakraui,
-      },
-    ],
-  },
-  {
-    title: "Guess The Word",
-    preview: GuessTheWord,
-    repository: "https://github.com/EdiedRamos/guess-the-word",
-    deployed: "https://ediedramos.github.io/guess-the-word/",
-    techList: [
-      {
-        name: "html",
-        Icon: Icons.html,
-      },
-      {
-        name: "css",
-        Icon: Icons.css,
-      },
-      {
-        name: "javascript",
-        Icon: Icons.javascript,
-      },
-    ],
-  },
-  {
     title: "Country Quiz",
     preview: CountryQuiz,
     repository: "https://github.com/EdiedRamos/country-quiz",
@@ -203,106 +156,6 @@ export const projectList: ProjectCardType[] = [
       {
         name: "sass",
         Icon: Icons.sass,
-      },
-    ],
-  },
-  {
-    title: "Url Shortener",
-    preview: UrlShortener,
-    repository: "https://github.com/EdiedRamos/url-shortening-api-landing-page",
-    deployed: "https://url-shortening-api-landing-page-tawny.vercel.app/",
-    techList: [
-      {
-        name: "react",
-        Icon: Icons.react,
-      },
-      {
-        name: "typescript",
-        Icon: Icons.typescript,
-      },
-      {
-        name: "sass",
-        Icon: Icons.sass,
-      },
-    ],
-  },
-  {
-    title: "Tic Tac Toe",
-    preview: TicTacToe,
-    repository: "https://github.com/EdiedRamos/tic-tac-toe",
-    deployed: "https://tic-tac-toe-kpse.vercel.app/",
-    techList: [
-      {
-        name: "react",
-        Icon: Icons.react,
-      },
-      {
-        name: "typescript",
-        Icon: Icons.typescript,
-      },
-      {
-        name: "sass",
-        Icon: Icons.sass,
-      },
-    ],
-  },
-  {
-    title: "Music Player",
-    preview: MusicPlayer,
-    repository: "https://github.com/EdiedRamos/music-player",
-    deployed: "https://ediedramos.github.io/music-player/",
-    techList: [
-      {
-        name: "react",
-        Icon: Icons.react,
-      },
-      {
-        name: "typescript",
-        Icon: Icons.typescript,
-      },
-      {
-        name: "sass",
-        Icon: Icons.sass,
-      },
-    ],
-  },
-  {
-    title: "IP Tracker",
-    preview: IpTracker,
-    repository: "https://github.com/EdiedRamos/ip-address-tracker",
-    deployed: "https://ip-address-tracker-zeta-flame.vercel.app/",
-    techList: [
-      {
-        name: "react",
-        Icon: Icons.react,
-      },
-      {
-        name: "typescript",
-        Icon: Icons.typescript,
-      },
-      {
-        name: "styled",
-        Icon: Icons.styled,
-      },
-    ],
-  },
-  {
-    title: "QR Generator",
-    preview: QrGenerator,
-    repository: "https://github.com/EdiedRamos/qr-generator",
-    deployed: "https://ediedramos.github.io/qr-generator/",
-    techList: [
-      {
-        name: "html",
-        Icon: Icons.html,
-      },
-      {
-        name: "css",
-        Icon: Icons.css,
-      },
-      {
-        name: "javascript",
-        Icon: Icons.javascript,
       },
     ],
   },
--- src/Resources/Projects/index.ts
+++ src/Resources/Projects/index.ts
@@ -1,13 +1,6 @@
-export { default as Boardify } from "./boardify.webp";
 export { default as CountryQuiz } from "./country-quiz.webp";
-export { default as GithubProfile } from "./github-profile.webp";
-export { default as GuessTheWord } from "./guess-the-word.webp";
 export { default as ImageUpload } from "./image-upload.webp";
-export { default as IpTracker } from "./ip-tracker.webp";
-export { default as MusicPlayer } from "./music-player.webp";
 export { default as MyTaskBoard } from "./my-task-board.webp";
 export { default as NoteCode } from "./notecode.webp";
-export { default as QrGenerator } from "./qr-generator.webp";
-export { default as TicTacToe } from "./tic-tac-toe.webp";
 export { default as UrlShortener } from "./url-shortener.webp";
 export { default as WorldRanks } from "./worldranks.webp";
