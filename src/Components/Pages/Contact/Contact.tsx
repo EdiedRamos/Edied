e2ff05923a06793014fd2f9b851f2293fcb592ef
EdiedRamos <edieisa22@gmail.com>
2024-07-13 21:44:01 -0500

    feat: set en|es for contact texts

--- src/Components/Pages/Contact/Contact.tsx
+++ src/Components/Pages/Contact/Contact.tsx
@@ -1,16 +1,19 @@
+import "./Contact.scss";
+
 import { SectionSetup, Title } from "@/Components/Atoms";
+
 import { ContactCards } from "@/Components/Molecules";
 import { ROUTE_URLS } from "@/Domain/Constants/urls";
-
-import "./Contact.scss";
-import { ContactText } from "@/Data/Text";
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
+  const { t } = useTranslation();
+
   return (
     <SectionSetup className="contact-section" id={ROUTE_URLS.CONTACT}>
       <div className="container">
         <article>
-          <Title>{ContactText.title}</Title>
+          <Title>{t("contact.title")}</Title>
           <div style={{ marginBottom: "4rem" }}>
             <ContactCards />
           </div>
--- src/Data/Text/contact.ts
+++
@@ -1,3 +0,0 @@
-export const ContactText = {
-  title: "Contact",
-};
