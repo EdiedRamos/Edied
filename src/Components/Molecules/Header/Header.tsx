27d7fc599b2ce3252e95937c349639e2afd63afd
EdiedRamos <edieisa22@gmail.com>
2024-07-26 20:07:08 -0500

    feat: add language button and update header design

--- src/Components/Molecules/Header/Header.tsx
+++ src/Components/Molecules/Header/Header.tsx
@@ -1,25 +1,40 @@
 import "./Header.scss";
 
 import { headerData } from "../../../Data/Header";
+import { useLanguage } from "@/Core";
 import { useTranslation } from "react-i18next";
 
 export const Header = () => {
   const { t } = useTranslation();
+  const { language, handleLanguage } = useLanguage();
 
   return (
     <header className="header">
+      <img
+        width={30}
+        height={30}
+        className="header__logo"
+        src="/public/logo.png"
+        alt="EdiedRamos logo"
+      />
       <ul className="header__list">
-        {headerData.map(({ id, name, redirect, Icon }) => (
+        {headerData.map(({ id, name, redirect }) => (
           <li className="header__list__item" key={id}>
             <div className="header__list__item__container">
               <a title={name} href={`#${redirect}`}>
-                <Icon />
+                <p>{t(`header.${name}`)}</p>
               </a>
-              <p>{t(`header.${name}`)}</p>
             </div>
           </li>
         ))}
       </ul>
+      <div className="header__language">
+        {language === "es" ? (
+          <button onClick={handleLanguage().toEnglish}>EN</button>
+        ) : (
+          <button onClick={handleLanguage().toSpanish}>ES</button>
+        )}
+      </div>
     </header>
   );
 };
