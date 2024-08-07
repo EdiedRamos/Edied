767b020f9ebed8779419d12658f1e8991705ef32
EdiedRamos <edieisa22@gmail.com>
2024-08-06 20:29:37 -0500

    refactor: remove navigation icons

--- src/Components/Icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { AboutIcon } from "./AboutIcon/AboutIcon";
-export { ContactIcon } from "./ContactIcon/ContactIcon";
-export { ProjectsIcon } from "./ProjectsIcon/ProjectsIcon";
-export { SkillsIcon } from "./SkillsIcon/SkillsIcon";
