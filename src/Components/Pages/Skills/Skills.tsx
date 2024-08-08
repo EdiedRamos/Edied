b47772fdf4f0a39b66f7fb90c4dad79180d0e050
EdiedRamos <edieisa22@gmail.com>
2024-08-08 16:37:02 -0500

    feat: add skills section title

--- src/Components/Pages/Skills/Skills.tsx
+++ src/Components/Pages/Skills/Skills.tsx
@@ -1,12 +1,16 @@
 import "./Skills.scss";
 
+import { SectionSetup, Title } from "@/Components/Atoms";
+
 import { ROUTE_URLS } from "@/Domain/Constants/urls";
-import { SectionSetup } from "@/Components/Atoms";
 import { SkillCards } from "@/Components/Molecules";
+import { useTranslation } from "react-i18next";
 
 const Skills = (): JSX.Element => {
+  const { t } = useTranslation();
   return (
     <SectionSetup className="skills-section" id={ROUTE_URLS.SKILLS}>
+      <Title>{t("header.skills")}</Title>
       <SkillCards />
     </SectionSetup>
   );
