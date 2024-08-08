6683efb887e6d3d34aa7ab2304ef4570c6067eaa
EdiedRamos <edieisa22@gmail.com>
2024-08-08 13:51:04 -0500

    feat: update introduce myself description keywords styles

--- src/Components/Molecules/IntroduceMyself/IntroduceMyself.tsx
+++ src/Components/Molecules/IntroduceMyself/IntroduceMyself.tsx
@@ -1,6 +1,7 @@
 import "./IntroduceMyself.scss";
 
 import { MyFace } from "@/Components/Atoms";
+import parse from "html-react-parser";
 import { useTranslation } from "react-i18next";
 
 export const IntroduceMyself = () => {
@@ -10,7 +11,7 @@ export const IntroduceMyself = () => {
     <div className="introduce-myself container">
       <div className="introduce-myself__text">
         <h1>{t("home.introduce.role")}</h1>
-        <p>{t("home.introduce.about")}</p>
+        <p>{parse(t("home.introduce.about"))}</p>
       </div>
       <MyFace />
     </div>
