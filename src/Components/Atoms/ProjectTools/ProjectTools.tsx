dea8e4cb5ccceb99a523e03f9dd648217d5eed57
EdiedRamos <edieisa22@gmail.com>
2024-08-08 22:13:53 -0500

    feat: update skills cards colors

--- src/Components/Atoms/ProjectTools/ProjectTools.tsx
+++ src/Components/Atoms/ProjectTools/ProjectTools.tsx
@@ -1,6 +1,6 @@
-import type { TechListType } from "../../../Types/projects";
-
 import "./ProjectTools.scss";
+
+import type { TechListType } from "../../../Types/projects";
 
 type Props = {
   techList: TechListType[];
@@ -10,11 +10,13 @@ export const ProjectTools = ({ techList }: Props) => {
   return (
     <div className="project-tech__container">
       {techList.map(({ name, Icon }) => (
-        <Icon
-          className="project-tech__icon"
-          key={name}
-          aria-label={`${name} icon`}
-        />
+        <span title={name}>
+          <Icon
+            className="project-tech__icon"
+            key={name}
+            aria-label={`${name} icon`}
+          />
+        </span>
       ))}
     </div>
   );
