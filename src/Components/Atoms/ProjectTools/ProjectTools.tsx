76cac4aa1e0f438e4647c4e6f1c22cb20ec06ffd
EdiedRamos <edieisa22@gmail.com>
2024-08-14 01:46:00 -0500

    fix: add key to ProjectTools loop

--- src/Components/Atoms/ProjectTools/ProjectTools.tsx
+++ src/Components/Atoms/ProjectTools/ProjectTools.tsx
@@ -10,7 +10,7 @@ export const ProjectTools = ({ techList }: Props) => {
   return (
     <div className="project-tech__container">
       {techList.map(({ name, Icon }) => (
-        <span title={name}>
+        <span key={name} title={name}>
           <Icon
             className="project-tech__icon"
             key={name}
